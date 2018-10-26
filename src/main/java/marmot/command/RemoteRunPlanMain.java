2d951ac9dae45d2593336dbeabc22b95fdcd9a05
kwlee <kwlee@etri.re.kr>
2018-10-26 21:49:41 +0900

    fix bug: append시 강제로 storeIntoDataSet 추가

--- src/main/java/marmot/command/RemoteRunPlanMain.java
+++ src/main/java/marmot/command/RemoteRunPlanMain.java
@@ -14,6 +14,8 @@ import marmot.MarmotRuntime;
 import marmot.Plan;
 import marmot.RecordSchema;
 import marmot.plan.STScriptPlanLoader;
+import marmot.proto.optor.OperatorProto;
+import marmot.proto.optor.StoreIntoDataSetProto;
 import marmot.remote.protobuf.PBMarmotClient;
 import utils.CommandLine;
 import utils.CommandLineParser;
@@ -52,27 +54,16 @@ public class RemoteRunPlanMain {
 			// 원격 MarmotServer에 접속.
 			PBMarmotClient marmot = MarmotClientCommands.connect(cl);
 			
-			File file = new File(cl.getArgument("plan_file"));
-			String script = IOUtils.toString(file);
-//			if ( cl.hasOptionValue("file") ) {
-//				script = IOUtils.toString(cl.getArgument("plan_file"));
-//			}
-//			else if ( cl.hasOptionValue("resource") ) {
-//				String name = cl.getString("resource");
-//				try ( InputStream is = Thread.currentThread()
-//											.getContextClassLoader()
-//											.getResourceAsStream(name) ) {
-//					if ( is == null ) {
-//						throw new IllegalArgumentException("invalid STScript: resource name=" + name);
-//					}
-//					script = IOUtils.toString(is, Charset.defaultCharset());
-//				}
-//			}
-//			else {
-//				try ( InputStream is = System.in ) {
-//					script = IOUtils.toString(is, Charset.defaultCharset());
-//				}
-//			}
+			String script;
+			if ( cl.getArgumentCount() > 0 ) {
+				File file = new File(cl.getArgument("plan_file"));
+				script = IOUtils.toString(file);
+			}
+			else {
+				try ( InputStream is = System.in ) {
+					script = IOUtils.toString(is, Charset.defaultCharset());
+				}
+			}
 			String planJson = STScriptPlanLoader.toJson(script);
 			
 			boolean showPlan = cl.hasOption("p");
@@ -92,7 +83,8 @@ public class RemoteRunPlanMain {
 	private static void createDataSet(MarmotRuntime marmot, Plan plan, CommandLine cl) {
 		String geomCol = cl.getOptionString("geom_col").getOrNull();
 		String srid = cl.getOptionString("srid").getOrNull();
-		
+		String outDsId = cl.getString("output");
+
 		boolean append = cl.hasOption("a");		
 		if ( !append ) {
 			List<DataSetOption> optList = Lists.newArrayList();
@@ -117,11 +109,33 @@ public class RemoteRunPlanMain {
 				optList.add(DataSetOption.COMPRESS);
 			}
 
-			String outDsId = cl.getString("output");
 			marmot.createDataSet(outDsId, plan, Iterables.toArray(optList, DataSetOption.class));
 		}
 		else {
+			plan = adjustPlanForStore(outDsId, plan);
 			marmot.execute(plan);
+		}
+	}
+	
+	private static Plan adjustPlanForStore(String dsId, Plan plan) {
+		OperatorProto last = plan.getLastOperator()
+								.getOrElseThrow(() -> new IllegalArgumentException("plan is empty"));
+		switch ( last.getOperatorCase() ) {
+			case STORE_INTO_DATASET:
+			case STORE_AS_CSV:
+			case STORE_INTO_JDBC_TABLE:
+			case STORE_AND_RELOAD:
+			case STORE_AS_HEAPFILE:
+				return plan;
+			default:
+				StoreIntoDataSetProto store = StoreIntoDataSetProto.newBuilder()
+																	.setId(dsId)
+																	.build();
+				OperatorProto op = OperatorProto.newBuilder().setStoreIntoDataset(store).build();
+				return Plan.fromProto(plan.toProto()
+											.toBuilder()
+											.addOperator(op)
+											.build());
 		}
 	}
 }
