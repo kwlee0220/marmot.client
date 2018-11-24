db44a66308327cc81d8c4434f1e47753cd457a80
kwlee <kwlee@etri.re.kr>
2018-11-24 21:51:23 +0900

    기능 수정

--- src/main/java/marmot/geo/geoserver/GSPFeatureSource.java
+++ src/main/java/marmot/geo/geoserver/GSPFeatureSource.java
@@ -6,6 +6,7 @@ import static marmot.optor.geo.SpatialRelation.INTERSECTS;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import java.util.function.Predicate;
@@ -23,6 +24,7 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Maps;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.prep.PreparedGeometry;
@@ -58,7 +60,9 @@ import utils.stream.FStream;
  */
 public class GSPFeatureSource extends ContentFeatureSource {
 	private static final Logger s_logger = LoggerFactory.getLogger(GSPFeatureSource.class);
-	private static final int MAX_CACHING_CLUSTERS = 5;
+	private static final int MAX_CACHE_SIZE = 10;
+	private static final int MAX_DISK_IO = 6;
+	private static final int MAX_NETWORK_IO = 4;
 
 	private final MarmotRuntime m_marmot;
 	private final GSPDataSetInfo m_dsInfo;
@@ -96,7 +100,6 @@ public class GSPFeatureSource extends ContentFeatureSource {
 		return this;
 	}
 	
-	
 	public RecordSet query(Envelope range) {
 		try {
 			if ( range.contains(m_dsInfo.getBounds()) ) {
@@ -109,60 +112,65 @@ public class GSPFeatureSource extends ContentFeatureSource {
 			}
 			
 			// 질의 영역과 겹치는 quad-key들과, 추정되는 결과 레코드를 계산한다.
-			Tuple2<List<SpatialClusterInfo>, Long> result = guessRelevants(range);
-			List<String> quadKeys = FStream.of(result._1)
-											.map(SpatialClusterInfo::getQuadKey)
-											.toList();
-			long totalGuess = result._2;
-			
-			// 추정된 레결과 레코드 수를 통해 샘플링 비율을 계산한다.
-			double sampleRatio = m_sampleCount.map(cnt -> (double)cnt / totalGuess)
-											.getOrElse(1d);
-			sampleRatio = sampleRatio > 1 ? 1 : sampleRatio;
+			QueryContext context = new QueryContext(m_dsInfo.getDataSet(), range, m_sampleCount);
+			List<String> quadKeys = context.streamQuadKeys().toList();
 			
 			Envelope overlap = range.intersection(m_dsInfo.getBounds());
 			double coverRatio = overlap.getArea() / m_dsInfo.getBounds().getArea();		
 			if ( coverRatio > 0.7 ) {
 				s_logger.info("too large for index, use full scan: id={}", m_dsId);
 				
-				return scanRange(range, sampleRatio);
+				return scanRange(range, context.getSampleRatio());
 			}
 			
 			// quad-key들 중에서 캐슁되지 않은 cluster들의 갯수를 구한다.
-			List<List<Record>> cachedParts = FStream.of(quadKeys)
-													.map(qkey -> m_cache.getIfPresent(m_dsId, qkey))
-													.filter(part -> part != null)
-													.toList();
-			int nonCachedCount = quadKeys.size() - cachedParts.size();
+			List<String> cachedKeys = FStream.of(quadKeys)
+											.filter(qkey -> m_cache.existsAtDisk(m_dsId, qkey))
+											.toList();
+			List<String> memCachedKeys = FStream.of(cachedKeys)
+												.filter(qkey -> m_cache.existsAtMemory(m_dsId, qkey))
+												.toList();
+			int nonCachedCount = quadKeys.size() - cachedKeys.size();
+			int diskIOCount = cachedKeys.size() - memCachedKeys.size();
 
-			String ratioStr = String.format("%.02f%%", sampleRatio*100);
+			String ratioStr = String.format("%.02f%%", context.getSampleRatio()*100);
 			if ( s_logger.isInfoEnabled() ) {
-				s_logger.info("status: ds_id={}, nclusters={}, ncaches={}, guess_count={}, ratio={}",
-								m_dsId, quadKeys.size(), cachedParts.size(), totalGuess, ratioStr);
+				s_logger.info("status: ds_id={}, cache={}:{}:{} guess_count={}, ratio={}",
+								m_dsId, memCachedKeys.size(), diskIOCount, nonCachedCount,
+								context.getTotalGuess(), ratioStr);
 			}
 			
-			if ( nonCachedCount > MAX_CACHING_CLUSTERS ) {
-				return indexedScan(range, sampleRatio);
+			if ( quadKeys.size() > MAX_CACHE_SIZE || diskIOCount > MAX_DISK_IO
+				|| nonCachedCount > MAX_NETWORK_IO ) {
+				return indexedScan(range, context.getSampleRatio());
 			}
 
-			s_logger.info("querying with caches: ds_id={}, ratio={}", m_dsId, ratioStr);
-			
-			Geometry key = GeoClientUtils.toPolygon(range);
-			PreparedGeometry pkey = PreparedGeometryFactory.prepare(key);
+			s_logger.debug("query on caches: ds_id={}, ratio={}", m_dsId, ratioStr);
 			FStream<Record> recStream = FStream.of(quadKeys)
-												.flatMapIterable(qk -> m_cache.get(m_dsId, qk))
-												.filter(r -> {
-													Geometry geom = r.getGeometry(m_gcInfo.name());
-													return pkey.intersects(geom);
-												});
-			if ( sampleRatio < 1 ) {
-				recStream = recStream.sample(sampleRatio);
-			}
+												.flatMap(qk -> query(context, qk));
+			
 			return RecordSets.from(m_dsInfo.getRecordSchema(), recStream);
 		}
 		catch ( Throwable e ) {
 			throw Throwables.toRuntimeException(Throwables.unwrapThrowable(e));
 		}
+	}
+	
+	private FStream<Record> query(QueryContext context, String quadKey) {
+		final PreparedGeometry pkey = context.getKey();
+		FStream<Record> matcheds = FStream.of(m_cache.get(m_dsId, quadKey))
+											.filter(r -> {
+												Geometry geom = r.getGeometry(m_gcInfo.name());
+												return pkey.intersects(geom);
+											});
+		if ( context.getSampleRatio() < 1 ) {
+			long takeCount = Math.round(context.getMatchCountGuess(quadKey)
+										* context.getSampleRatio() * 1.05);
+			matcheds = matcheds.sample(context.getSampleRatio())
+								.take(takeCount);
+		}
+		
+		return matcheds;
 	}
 
 	@Override
@@ -243,24 +251,84 @@ public class GSPFeatureSource extends ContentFeatureSource {
 		return builder;
 	}
 	
-	private Tuple2<List<SpatialClusterInfo>, Long> guessRelevants(Envelope range) {
-		List<SpatialClusterInfo> infos = m_dsInfo.getDataSet()
-												.querySpatialClusterInfo(range);
+	static class QueryContext {
+		static class Match {
+			private SpatialClusterInfo m_info;
+			private int m_guessMatchCount;
+			
+			private Match(SpatialClusterInfo info, int count) {
+				m_info = info;
+				m_guessMatchCount = count;
+			}
+		}
 		
-		List<Tuple2<SpatialClusterInfo,Long>> relevants = FStream.of(infos)
+		private final DataSet m_ds;
+		private final Map<String,Match> m_matches;
+		private final long m_totalGuess;
+		private final double m_sampleRatio;
+		private final PreparedGeometry m_pkey;
+		
+		private QueryContext(DataSet ds, Envelope range, Option<Long> sampleCount) {
+			m_ds = ds;
+			
+			// 질의 영역과 겹치는 quad-key들과, 추정되는 결과 레코드를 계산한다.
+			List<Tuple2<SpatialClusterInfo,Integer>> relevants = guessRelevants(ds, range);
+
+			long total = 0;
+			m_matches = Maps.newHashMap();
+			for ( Tuple2<SpatialClusterInfo,Integer> match: relevants ) {
+				m_matches.put(match._1.getQuadKey(), new Match(match._1, match._2));
+				total += match._2;
+			}
+			m_totalGuess = total;
+			
+			// 추정된 레결과 레코드 수를 통해 샘플링 비율을 계산한다.
+			double sampleRatio = sampleCount.map(cnt -> (double)cnt / m_totalGuess)
+											.getOrElse(1d);
+			m_sampleRatio = sampleRatio > 1 ? 1 : sampleRatio;
+			
+			Geometry key = GeoClientUtils.toPolygon(range);
+			m_pkey = PreparedGeometryFactory.prepare(key);
+		}
+		
+		DataSet getDataSet() {
+			return m_ds;
+		}
+		
+		PreparedGeometry getKey() {
+			return m_pkey;
+		}
+		
+		long getTotalGuess() {
+			return m_totalGuess;
+		}
+		
+		int getMatchCountGuess(String quadKey) {
+			return m_matches.get(quadKey).m_guessMatchCount;
+		}
+		
+		double getSampleRatio() {
+			return m_sampleRatio;
+		}
+		
+		FStream<String> streamQuadKeys() {
+			return FStream.of(m_matches.values())
+							.map(m -> m.m_info.getQuadKey());
+		}
+		
+		private static List<Tuple2<SpatialClusterInfo,Integer>> guessRelevants(DataSet ds, Envelope range) {
+			List<SpatialClusterInfo> infos = ds.querySpatialClusterInfo(range);
+			return FStream.of(infos)
 							.map(idx -> Tuple.of(idx, guessCount(range, idx)))
 							.filter(t -> t._2 > 0)
 							.toList();
-		long total = FStream.of(relevants).mapToLong(r -> r._2).sum();
-		List<SpatialClusterInfo> domain = FStream.of(relevants).map(r -> r._1).toList();
+		}
 		
-		return Tuple.of(domain, total);
-	}
-	
-	private long guessCount(Envelope boundsWgs84, SpatialClusterInfo info) {
-		Envelope bounds = info.getDataBounds().intersection(info.getTileBounds());
-		double ratio =  boundsWgs84.intersection(bounds).getArea() / bounds.getArea();
-		return Math.round(info.getRecordCount() * ratio);
+		private static int guessCount(Envelope key, SpatialClusterInfo info) {
+			Envelope domain = info.getDataBounds().intersection(info.getTileBounds());
+			double ratio =  key.intersection(domain).getArea() / domain.getArea();
+			return (int)Math.round(info.getRecordCount() * ratio);
+		}
 	}
 	
 	private RecordSet indexedScan(Envelope range, double sampleRatio) {
