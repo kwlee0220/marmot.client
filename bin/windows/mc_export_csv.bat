0ed265e4f1e9f68dd346037cb32a59c9bcb79248
kwlee <kwlee@etri.re.kr>
2019-08-23 15:41:59 +0900

    fix a script bug

--- bin/windows/mc_export_csv.bat
+++ bin/windows/mc_export_csv.bat
@@ -1,5 +1,5 @@
 @echo off
 
 java -cp %MARMOT_CLIENT_HOME%/bin/marmot.client.jar ^
-marmot.command.RemoteImportCsvFileMain ^
+marmot.command.RemoteExportAsCsvFileMain ^
 -host %MARMOT_HOST% -port %MARMOT_PORT% %*
