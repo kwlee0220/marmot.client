1d5d0b80e76df31cc4055727004a6a05c12c4bf1
kwlee <kwlee@etri.re.kr>
2019-09-30 16:19:54 +0900

    square-grid 분석 기능 수정

--- src/main/java/marmot/geo/command/RemoteBuildSquareGridMain.java
+++ src/main/java/marmot/geo/command/RemoteBuildSquareGridMain.java
@@ -1,12 +1,17 @@
 package marmot.geo.command;
 
+import marmot.GeometryColumnInfo;
+import marmot.MarmotRuntime;
+import marmot.PlanBuilder;
 import marmot.command.MarmotClientCommands;
-import marmot.command.MarmotConnector;
-import marmot.remote.protobuf.PBMarmotClient;
+import marmot.command.PlanBasedMarmotCommand;
+import marmot.optor.geo.SquareGrid;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Help.Ansi;
 import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 /**
  * 
@@ -16,9 +21,28 @@ import picocli.CommandLine.Mixin;
 		parameterListHeading = "Parameters:%n",
 		optionListHeading = "Options:%n",
 		description="build a square-grid for a dataset")
-public class RemoteBuildSquareGridMain extends BuildSquareGridCommand {
-	@Mixin private MarmotConnector m_connector;
+public class RemoteBuildSquareGridMain extends PlanBasedMarmotCommand {
+	@Mixin private Params m_params;
+	
+	public static class Params {
+		@Parameters(paramLabel="dataset", index="0", arity="1..1",
+					description={"dataset id for grid boundary"})
+		private String m_input;
+		
+		@Parameters(paramLabel="grid_dataset", index="1", arity="1..1",
+					description={"square-grid dataset"})
+		private String m_output;
 
+		@Option(names={"-grid"}, paramLabel="grid_expr", required=true, description={"grid expression"})
+		public void setSquareGrid(String expr) {
+			m_grid = SquareGrid.parseString(expr);
+		}
+		private SquareGrid m_grid;
+
+		@Option(names={"-o", "-overlap"}, description={"skip un-overlapped cells)"})
+		private boolean m_overlap = false;
+	}
+	
 	public static final void main(String... args) {
 		MarmotClientCommands.configureLog4j();
 
@@ -30,13 +54,32 @@ public class RemoteBuildSquareGridMain extends BuildSquareGridCommand {
 			if ( commandLine.isUsageHelpRequested() ) {
 				commandLine.usage(System.out, Ansi.OFF);
 			}
-
-			PBMarmotClient marmot = cmd.m_connector.connect();
-			cmd.accept(marmot);
+			else {
+				
+				cmd.run("spatial_join", cmd.m_params.m_output);
+			}
 		}
 		catch ( Exception e ) {
 			System.err.printf("failed: %s%n%n", e);
 			commandLine.usage(System.out, Ansi.OFF);
+		}
+	}
+
+	@Override
+	protected PlanBuilder addLoad(MarmotRuntime marmot, PlanBuilder builder) {
+		GeometryColumnInfo gcInfo = marmot.getDataSet(m_params.m_input)
+											.getGeometryColumnInfo();
+		setGeometryColumnInfo(gcInfo);
+		String prjExpr = String.format("cell_geom as %s,cell_id,cell_pos", gcInfo.name());
+		
+		if ( m_params.m_overlap ) {
+			return builder.load(m_params.m_input)
+							.assignGridCell(gcInfo.name(), m_params.m_grid, false)
+							.project(prjExpr)
+							.distinct("cell_id");
+		}
+		else {
+			return builder.loadGrid(m_params.m_grid);
 		}
 	}
 }
